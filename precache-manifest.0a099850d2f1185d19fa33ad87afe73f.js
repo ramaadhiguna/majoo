b269c91793f70694fd346308d29ed37e58b09765
ramaadhiguna <ramaadhiguna@gmail.com>
2021-01-12 13:27:15 +0700

    Updates

---
+++ precache-manifest.0a099850d2f1185d19fa33ad87afe73f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501e2955b7c11f44608b15e775e6c219",
+    "url": "/majoo/index.html"
+  },
+  {
+    "revision": "df1c07af685870337999",
+    "url": "/majoo/static/css/2.f0785429.chunk.css"
+  },
+  {
+    "revision": "75dc99dff4478206181f",
+    "url": "/majoo/static/css/main.1d99bac7.chunk.css"
+  },
+  {
+    "revision": "df1c07af685870337999",
+    "url": "/majoo/static/js/2.5e83254e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/majoo/static/js/2.5e83254e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75dc99dff4478206181f",
+    "url": "/majoo/static/js/main.9531b3c2.chunk.js"
+  },
+  {
+    "revision": "3062cd963f5f3e06b5eb",
+    "url": "/majoo/static/js/runtime-main.a46e32ff.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/majoo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/majoo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/majoo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/majoo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/majoo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
